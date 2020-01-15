62b9ebec17c16bfce776683bd367a5a7981dc9b6
GentaShibasaki <genta.shibasaki.78@gmail.com>
2020-01-16 01:52:14 +0900

    finished Populatron

--- populatron.js
+++ populatron.js
@@ -1,5 +1,24 @@
 const fs = require("fs");
+let actual = 0;
+fs.readFile(`./cities.csv`, `utf-8`, (error, data) => {
+  if (error) throw error;
+  let contents = data.split("\n");
+  contents.shift();
+  actual = contents
+    .map((item) => +item.split(`,`)[2])
+    .reduce((acc, cur) => acc + cur);
+});
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (typeof actual === "number") {
+          resolve(onFinished(actual));
+        } else {
+          reject(onFinished("Error!"));
+        }
+      }, 1000);
+    });
+  },
 };
